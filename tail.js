d39a8b6956d27db86852ec0ad863d1d69c94cd4e
Daniel Lu <daniel_lu@hotmail.com>
2021-11-22 20:54:21 -0500

    added tail.js

---
+++ tail.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log(String.fromCodePoint(0x1F628) + `Assertion Failed: ${actual} !== ${expected}`);
+      }
+    }
+    console.log(String.fromCodePoint(0x1F60D) + `Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  let newArr = arr.slice(1);
+  return newArr;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
