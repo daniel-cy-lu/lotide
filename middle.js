0869ba6fc9e1e9726d31ca3edd170adb0741520e
Daniel Lu <daniel_lu@hotmail.com>
2021-11-29 18:45:21 -0500

    middle.js clean-up assignment

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if ((!arr1) || (!arr2)) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-};
-
-
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(String.fromCodePoint(0x1F628) + `Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x1F60D) + `Assertion Passed: ${actual} === ${expected}`);
-  }
-};
 
 const middle = function(array) {
   if (array.length === 0 || array.length === 1) {
@@ -32,5 +11,4 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7]));
+module.exports = middle;
