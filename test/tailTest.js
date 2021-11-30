224da5ca4944d8f83dd93f1a0f7f963001c5e33c
Daniel Lu <daniel_lu@hotmail.com>
2021-11-29 21:16:39 -0500

    mocha and chai for headTest.js and tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,11 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertArraysEqual(tail(words), ['Lighthouse', 'Labs']);
+describe("#tail", () => {
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
